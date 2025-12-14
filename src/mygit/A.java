dcfebad57ca744bc06f2310b766cc180f7c4c36e
p1001q <p1001q@gmail.com>
2025-12-14 22:57:35 +0900

    Bob first commit (6)

--- src/mygit/A.java
+++ src/mygit/A.java
@@ -1,5 +1,8 @@
 package mygit;
 
 public class A {
+	public static void main(String[] args) {
+		System.out.println("A: changed on Bob");
 
+	}
 }
--- src/mygit/B.java
+++ src/mygit/B.java
@@ -1,5 +1,8 @@
 package mygit;
 
 public class B {
+	public static void main(String[] args) {
+		System.out.println("C: changed on Bob");
 
+	}
 }
